beebbe5b4c04a2bd5abae9a1d14213fc372902d6
Hari Kumar Valluru <anil14353@gmail.com>
2018-02-01 21:45:37 +0530

    Extending For Loops

--- ForLoops/forLoops.py
+++ ForLoops/forLoops.py
@@ -17,3 +17,22 @@ for i in range(0, len(number)):
         cleanedNumber = cleanedNumber + number[i]
 newNumber = int(cleanedNumber)
 print(newNumber)
+
+number = "9,114,665,224,744,811"
+cleanedNumber = ""
+for char in number:
+    if char in '0123456789':
+        cleanedNumber = cleanedNumber + char
+newNumber = int(cleanedNumber)
+print(newNumber)
+
+for state in ['not pinin','no more','a stiff','bereft of life']:
+    print("This parrot is "+state)
+
+for i in range(0,100,5):
+    print("i is {}".format(i))
+
+for i in range(1,12):
+    for j in range(1,13):
+        print("{1} times {0} is {2}".format(i,j,i*j))
+    print("===============")
