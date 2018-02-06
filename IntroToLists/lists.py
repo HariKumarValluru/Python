fd8606e32375f4a382dab33cc755b2d231e73432
Hari Kumar Valluru <anil14353@gmail.com>
2018-02-06 23:04:39 +0530

    Intro to Lists

---
+++ IntroToLists/lists.py
@@ -0,0 +1,16 @@
+# ipAddress = input("Please enter an IP address: ")
+# print(ipAddress.count("."))
+
+parrot_list = ['nom pinin', 'no more', 'a stiff', 'bereft of live']
+
+parrot_list.append('A Norwegian Blue')
+
+for state in parrot_list :
+    print("The parrot is " + state)
+
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+numbers = even + odd
+# numbers.sort()
+# print(numbers)
+print(sorted(numbers))
