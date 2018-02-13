44087e3a777e359a43d96d6b08a5ee406aa2bf7b
Hari Kumar Valluru <anil14353@gmail.com>
2018-02-13 17:44:19 +0530

    Iterators

---
+++ Iterators/Iterators.py
@@ -0,0 +1,12 @@
+string = "0123456789"
+# for char in string:
+#     print(char)
+my_iterator = iter(string)
+print(my_iterator)
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
