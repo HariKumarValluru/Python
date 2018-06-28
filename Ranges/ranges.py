35b41f2f2a64f4415a89f373c999d0a7c1e1659a
Hari Kumar Valluru <anil14353@gmail.com>
2018-06-28 19:12:14 +0530

    ranges

---
+++ Ranges/ranges.py
@@ -0,0 +1,8 @@
+decimals = range(0,100)
+print(decimals)
+my_range = decimals[3:40:3]
+print(my_range)
+print(my_range == decimals[3:40:3])
+print(range(0,5,2) == range(0,6,2))
+print(list(range(0,5,2)))
+print(list(range(0,6,2)))
