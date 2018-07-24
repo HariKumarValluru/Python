07ae0af850821b8f6cca92a499e3f1ac60f039f9
Hari Kumar Valluru <anil14353@gmail.com>
2018-07-24 10:08:54 +0530

    ranges example

--- Ranges/ranges.py
+++ Ranges/ranges.py
@@ -6,3 +6,9 @@ print(my_range == decimals[3:40:3])
 print(range(0,5,2) == range(0,6,2))
 print(list(range(0,5,2)))
 print(list(range(0,6,2)))
+
+r = range(0,100)
+print(r)
+
+for i in r[::-2]:
+    print(i)
